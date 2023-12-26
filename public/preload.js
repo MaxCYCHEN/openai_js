20d7dbc89e13779ab77fb1dcb2ff03f1e31099da
CMWU <CMWU1@nuvoton.com>
2023-12-26 17:49:31 +0800

    Remove require('fs-extra')

--- public/preload.js
+++ public/preload.js
@@ -2,7 +2,6 @@
 // It has the same sandbox as a Chrome extension.
 const { contextBridge, ipcRenderer } = require("electron");
 const child_process = require('node:child_process');
-const fs = require('fs-extra');
 // const rimraf =  require ( 'rimraf' );
 
 // As an example, here we use the exposeInMainWorld API to expose the browsers
@@ -11,6 +10,5 @@ const fs = require('fs-extra');
 process.once("loaded", () => {
   contextBridge.exposeInMainWorld("versions", process.versions);
   contextBridge.exposeInMainWorld("child_process", child_process);
-  contextBridge.exposeInMainWorld("fs", fs);
   contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer);
 });

40322d3864a69894baf418a0eb75006d2c57f6e4
CMWU <CMWU1@nuvoton.com>
2023-12-26 17:06:14 +0800

    Support electron.

---
+++ public/preload.js
@@ -0,0 +1,16 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+const { contextBridge, ipcRenderer } = require("electron");
+const child_process = require('node:child_process');
+const fs = require('fs-extra');
+// const rimraf =  require ( 'rimraf' );
+
+// As an example, here we use the exposeInMainWorld API to expose the browsers
+// and node versions to the main window.
+// They'll be accessible at "window.versions".
+process.once("loaded", () => {
+  contextBridge.exposeInMainWorld("versions", process.versions);
+  contextBridge.exposeInMainWorld("child_process", child_process);
+  contextBridge.exposeInMainWorld("fs", fs);
+  contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer);
+});
